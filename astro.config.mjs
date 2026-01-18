4acd318a03a09c793793b92a10ab35a2ef3c6479
jfkeci <ja.lip132@gmail.com>
2026-01-18 13:01:17 +0100

    feature: gh pages?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jfkeci.github.io',
+	base: '/x-db',
 	integrations: [mdx(), sitemap()],
 });
